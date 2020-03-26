739d1e6362e0ae4e7da119cdde6f842e5187bfa4
Daniel <52752918+lisdanya@users.noreply.github.com>
2020-03-26 18:23:09 +0200

    adding command reading

--- lab_OP_3_var0.py
+++ lab_OP_3_var0.py
@@ -1,18 +1,10 @@
-oper = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
-        '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+import sys
 
 
-def parse(formula_string):
-    numbers = ''
-    for s in formula_string:
-        if s in '1234567890.':
-            numbers += s
-        elif numbers:
-            yield float(numbers)
-            numbers = ''
-        if s in oper or s in "()":
-            yield s
-    if numbers:
-        yield float(numbers)
-
-
+if len(sys.argv) > 1:
+    formulaa = ""
+    for i in range(1, len(sys.argv)):
+        formulaa = str(formulaa) + str(sys.argv[i])
+    print("Result: ")
+else:
+    print("ERROR")
