db8e3c9b84dbd4d41809b38e34150b28bff28732
MIDAS26 <56016042+MIDAS26@users.noreply.github.com>
2020-03-26 18:28:55 +0200

    add function calculation

--- lab_OP_3_var0.py
+++ lab_OP_3_var0.py
@@ -1,5 +1,33 @@
 import sys
 
+oper = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+        '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+
+
+def parse(formula_string):
+    numbers = ''
+    for s in formula_string:
+        if s in '1234567890.':
+            numbers += s
+        elif numbers:
+            yield float(numbers)
+            numbers = ''
+        if s in oper or s in "()":
+            yield s
+    if numbers:
+        yield float(numbers)
+
+
+def calc(polish):
+    stack = []
+    for token in polish:
+        if token in oper:
+            y = stack.pop()
+            x = stack.pop()
+            stack.append(oper[token][1](x, y))
+        else:
+            stack.append(token)
+    return stack[0]
 
 if len(sys.argv) > 1:
     formulaa = ""
